dca49de689db9f02c45a36bcaed7dd7f2092d600
Guy J Grigsby <guy.grigsby@gmail.com>
2021-05-31 03:37:39 -0600

    still trying

--- src/main/java/dev/cryptopig/trees/Log.java
+++ src/main/java/dev/cryptopig/trees/Log.java
@@ -4,10 +4,12 @@ import static net.minecraft.util.ActionResultType.PASS;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalBlock;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.state.DirectionProperty;
 import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
@@ -17,11 +19,12 @@ import net.minecraft.world.World;
  * @author CryptoPig
  */
 
-public class Log extends Block {
+public class Log extends HorizontalBlock {
     public static final DirectionProperty FACING = BlockStateProperties.FACING;
 
     public Log(Block.Properties props) {
         super(props);
+        this.setDefaultState(this.getDefaultState().with(HORIZONTAL_FACING, Direction.NORTH));
     }
 
     public ActionResultType onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player,
